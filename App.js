d42be892507b3baf8476760141ee8447246a5a7b
Joe Ulyatt <joewhocodes@gmail.com>
2023-06-30 11:45:04 +0200

    update Header styling

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Footer />
-    </View>
-  );
+	return (
+		<>
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: '#495E57',
+				}}>
+				<Header />
+			</View>
+			<View style={{ backgroundColor: '#495E57' }}>
+				<Footer />
+			</View>
+		</>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,20 @@
-import * as React from 'react';
 import { View, Text } from 'react-native';
 
 export default function Header() {
-    return (
-      <View
-        style={{
-          backgroundColor: '#F4CE14',
-          marginBottom: 10,
-        }}>
-        <Text
-          style={{
-            fontSize: 18,
-            color: 'black',
-            textAlign: 'center',
-          }}>
-          All rights reserved by Little Lemon, 2022{' '}
-        </Text>
-      </View>
-    );
-  }
+	return (
+		<View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
+			<Text
+				style={{
+					padding: 40,
+					fontSize: 30,
+					color: 'black',
+					textAlign: 'center',
+				}}
+				numberOfLines={3}
+			>
+				Welcome To
+				<Text style={{ fontWeight: 'bold' }}> Little Lemon</Text>
+			</Text>
+		</View>
+	);
+}
